7b91cb0fbdc9210d3937fca43d309b3b273d8d99
Mark Baker <mrbaker1917>
2025-12-23 17:15:20 -0800

    sets up repl

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
-    fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+	for true {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		input := scanner.Text()
+		cleaned_input := cleanInput(input)
+		fmt.Printf("Your command was: %s\n", cleaned_input[0])
+	}
 }
